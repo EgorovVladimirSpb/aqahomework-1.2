5630653d28d0de5f3dae3b7bcfda36fc6f84c820
Egorov Vladimir <egorov.vladimir.spb@gmail.com>
2021-03-02 13:29:02 +0300

    Added RUB to Tests

--- src/test/java/ru/netology/rest/MobileBankApiTestV4MofidiedForHomework.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4MofidiedForHomework.java
@@ -23,7 +23,7 @@ class MobileBankApiTestV4MofidiedForHomework {
       .then()
               .statusCode(200)
               .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-              .body("[0].currency", equalTo("RUR"))
+              .body("[0].currency", equalTo("RUB"))
       ;
     }
 }
